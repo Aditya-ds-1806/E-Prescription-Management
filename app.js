73feec26f73edfee08cc2515bd2350904501c209
Aditya D.S <adithya18062000@gmail.com>
2020-03-30 21:00:31 +0530

    Can generate prescription from user input

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/prescription', function (req, res) {
-    res.render('prescription');
+app.post('/prescription', function (req, res) {
+    res.render('prescription', { data: req.body });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
