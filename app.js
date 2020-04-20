7ee6b1525f42c34be8fac87c6b6ca52febcdbbad
Aditya D.S <adithya18062000@gmail.com>
2020-04-20 17:54:01 +0530

    .

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// Controllers
-const PrescriptionController = require('./controllers/prescription');
+const basicRoutes = require('./routes/basic-routes');
 
 if (process.env.NODE_ENV === 'development') {
     const env = require('dotenv');
@@ -12,24 +11,21 @@ if (process.env.NODE_ENV === 'development') {
 
 const app = express();
 
-const conn = mongoose.connect(process.env.DATABASEURL, {
+mongoose.connect(process.env.DATABASEURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
+    .then(() => console.log('Connected to DB'))
+    .catch((err) => {
+        console.log("Couldn't connect to DB");
+        console.error(err);
+    });
 
-mongoose.connection.on('open', function () {
-    console.log('Connected to DB');
-});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    res.render('index');
-});
-
-app.post('/prescription', PrescriptionController.renderPrescription);
+app.use(basicRoutes);
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
     console.log('Server listening on port:', process.env.PORT || 3000);
