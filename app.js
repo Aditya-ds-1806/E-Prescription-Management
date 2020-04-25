b3057beea26d43e8486205e2e331f4c08b2ed273
Aditya D.S <adithya18062000@gmail.com>
2020-04-26 02:01:56 +0530

    Set up passportjs auth and cookies

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const passportSetup = require('./config/passport-setup');
+const cookieSession = require('cookie-session');
 
 const basicRoutes = require('./routes/basic-routes');
+const authRoutes = require('./routes/auth-routes');
 
 if (process.env.NODE_ENV === 'development') {
     const env = require('dotenv');
@@ -23,9 +27,20 @@ mongoose.connect(process.env.DATABASEURL, {
 
 
 app.set('view engine', 'ejs');
+
+app.use(
+    cookieSession({
+        name: 'DSP',
+        maxAge: 24 * 60 * 60 * 1000,
+        keys: ['IIITDMKancheepuram']
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(basicRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
     console.log('Server listening on port:', process.env.PORT || 3000);
