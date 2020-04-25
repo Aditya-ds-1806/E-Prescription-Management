eb6d76fd38fe2c88e7847e2fe1dd95ea7d1f40d9
Aditya D.S <adithya18062000@gmail.com>
2020-04-26 01:40:21 +0530

    created controller for handling doctor related tasks

---
+++ controllers/doctor.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const doctorSchema = require('../models/doctor');
+
+const Doctor = mongoose.model('Doctor', doctorSchema);
+
+exports.addNewDoctor = async function (profile, done) {
+    const user = await Doctor.findOne({ googleID: profile.id });
+    if (user) {
+        console.log('User exists in DB: ', user.email);
+        done(null, user);
+    } else {
+        var newDoctor = new Doctor({
+            name: profile.name.givenName,
+            googleID: profile.id,
+            image: profile.photos[0].value,
+            email: profile.emails[0].value
+        });
+        const addedUser = await newDoctor.save().catch((err) => {
+            console.log(err);
+            res.redirect('/');
+        });
+        console.log(addedUser.name + ' was added to the DB');
+        done(null, addedUser);
+    }
+}
+
+exports.updateProfile = function (details, id) {
+    Doctor.findByIdAndUpdate(id, details, { useFindAndModify: false }, function (err, updatedProfile) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Details updated succesfully');
+            console.log(updatedProfile);
+        }
+    });
+}
