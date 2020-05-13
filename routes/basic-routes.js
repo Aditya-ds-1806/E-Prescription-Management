3b4241b35276b460c47918755b6e9e35ee464fb5
Aditya D.S <adithya18062000@gmail.com>
2020-05-14 00:50:00 +0530

    moved FFT code to new controller

--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -17,57 +17,27 @@ router.get('/verify', user.isLoggedIn, function (req, res) {
 });
 
 router.post('/verify', user.isLoggedIn, function (req, res) {
-    const tempPath = __dirname + "//..//temp";
+    const tempPath = __dirname + "//..//temp//";
+    const downloadPath = tempPath + "uploaded//";
     const form = formidable({
         multiples: true,
-        uploadDir: tempPath,
+        uploadDir: downloadPath,
         keepExtensions: true
     });
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
         if (err) {
             console.error('Error while parsing pdf', err);
             throw err;
         } else if (files.prescription.type !== "image/png") {
             res.send("Incorrect file format, only PNG supported!");
         } else {
-            // const prscID = fields.prscID;
-            const Jimp = require('jimp');
-            Jimp.read(files.prescription.path)
-                .then(image => {
-                    image.greyscale();
-                    image.resize(Jimp.AUTO, 512);
-                    return image.writeAsync(files.prescription.path);
-                })
-                .then(async image => {
-                    console.log("Done greyscaling and editing");
-                    const Fourier = require('../controllers/fourier/fourier');
-                    var pixelMatrix = [];
-                    var fft = [];
-                    image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
-                        pixelMatrix.push(this.bitmap.data[idx]);
-                        if (x === image.bitmap.width - 1 && y === image.bitmap.height - 1) {
-                            const lowerPowerOf2 = Math.floor(Math.log2(pixelMatrix.length));
-                            const higherPowerOf2 = lowerPowerOf2 + 1;
-                            const zeroPadLength = Math.pow(2, higherPowerOf2) - pixelMatrix.length;
-                            for (let i = 0; i < zeroPadLength; i++) {
-                                pixelMatrix.push(255);
-                            }
-                        }
-                    });
-                    console.log("Image scan successful, retreived Image matrix", pixelMatrix.length);
-                    Fourier.transform(pixelMatrix, fft);
-                    const dims = [image.bitmap.width, image.bitmap.height];
-                    pixelMatrix = Fourier.shift(pixelMatrix, dims);
-                    console.log("Computed fft");
-                    image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
-                        this.setPixelColor(pixelMatrix[x + y * x], x, y);
-                    });
-                    image.write(tempPath + "//fft.png");
-                    res.sendFile('fft.png', { root: tempPath });
-                })
-                .catch(err => {
-                    console.error(err);
-                });
+            const FFTUtils = require('../controllers/fourier/FFTUtils');
+            const prscID = fields.prscID;
+            var uploadedFilePath = downloadPath + prscID + ".png";
+            fs.renameSync(files.prescription.path, uploadedFilePath);
+            var frImage = await FFTUtils.getFourierImage(uploadedFilePath);
+            const diffPercent = await FFTUtils.compareImages(tempPath + prscID + ".png", frImage);
+            res.send(diffPercent === 0);
         }
     });
 });
@@ -89,18 +59,27 @@ router.post('/prescription', user.isLoggedIn, user.hasUpdatedDetails, async func
     res.redirect('/prescription/' + savedPrescription.id);
 });
 
-router.post('/prscImg', function (req, res) {
+router.post('/prscImg', user.isLoggedIn, user.hasUpdatedDetails, async function (req, res) {
+    const FFTUtils = require('../controllers/fourier/FFTUtils')
+    var fileName = req.body.prscID + ".png";
+    const tempPath = __dirname + "//..//temp//";
+    const uploadedFilePath = tempPath + "prsc-" + fileName;
+    const savedFilePath = tempPath + fileName;
     var base64EncodedImage = req.body.image;
-    const fileName = req.body.prscID;
+
     base64EncodedImage = base64EncodedImage.replace(/^data:image\/png;base64,/, "");
-    fs.writeFileSync(__dirname + "//..//temp//" + fileName + ".png", base64EncodedImage, 'base64');
+    fs.writeFileSync(uploadedFilePath, base64EncodedImage, 'base64');
+    var fourierImage = await FFTUtils.getFourierImage(uploadedFilePath);
+    fourierImage.write(savedFilePath);
     console.log("Saved Image");
     res.sendStatus(200);
 });
 
-router.get('/download', function (req, res) {
-    var fileName = req.query.id + ".png";
+router.get('/download', user.isLoggedIn, user.hasUpdatedDetails, function (req, res) {
+    const fs = require('fs');
+    var fileName = "prsc-" + req.query.id + ".png";
     res.download(__dirname + "//..//temp//" + fileName);
+    // fs.unlinkSync(__dirname + "//..//temp//" + fileName);
 });
 
 router.get('/profile', user.isLoggedIn, function (req, res) {
