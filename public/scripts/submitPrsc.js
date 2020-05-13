f1afcd0f64a31082f7232136c97565dbad91737a
Aditya D.S <adithya18062000@gmail.com>
2020-05-14 02:50:00 +0530

    prescriptions can be generated on any device, added spinner

--- public/scripts/submitPrsc.js
+++ public/scripts/submitPrsc.js
@@ -1,25 +1,24 @@
 const downloadButton = document.querySelector("#download");
+const spinner = document.querySelector("#spinner");
 const prscID = document.querySelector("#prscID").textContent.substring(18);
 
-downloadButton.addEventListener('click', function (e) {
+downloadButton.addEventListener('click', async function (e) {
     e.preventDefault();
+    spinner.classList.remove('d-none');
     var scrollPos = window.scrollY;
     window.scroll(0, 0);
-    html2canvas(document.querySelector("article"), {
-        x: 300,
-        width: 900
-    }).then(function (canvas) {
-        window.scroll(0, scrollPos);
-        var base64EncodedImg = canvas.toDataURL('image/png');
-        $.ajax({
-            type: "POST",
-            url: "/prscImg",
-            data: { image: base64EncodedImg, prscID: prscID },
-            success: function (res) {
-                console.log(res);
-                var win = window.open();
-                win.location = "/download?id=" + prscID;
-            }
-        });
+    var canvas = await html2canvas(document.querySelector("article"));
+    window.scroll(0, scrollPos);
+    var base64EncodedImg = canvas.toDataURL('image/png');
+    $.ajax({
+        type: "POST",
+        url: "/prscImg",
+        data: { image: base64EncodedImg, prscID: prscID },
+        success: function (res) {
+            spinner.classList.add('d-none');
+            console.log(res);
+            var win = window.open();
+            win.location = "/download?id=" + prscID;
+        }
     });
 });
