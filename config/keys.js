c63f4bc15778dc53e2ac7f7bb6cf6c6ee089eb06
Aditya D.S <adithya18062000@gmail.com>
2020-05-14 02:55:50 +0530

    addding keys.js to gitignore after heroku deploy

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-var keys = {
-    google: {
-        clientID: '266651871871-0gjo44076lp89jj3mqi8h95r1ui1ddq8.apps.googleusercontent.com',
-        clientSecret: 'kTUlOqG6gMLEcl5-usWa_EaZ',
-        callbackURL: '/auth/google/redirect'
-    }
-};
-
-module.exports = keys;
