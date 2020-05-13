fbfd3902663e7e1a2ffefaad4cdbb28dfb8b17c1
Aditya D.S <adithya18062000@gmail.com>
2020-05-13 16:48:08 +0530

    Generate image, save on server, and prompt download

---
+++ public/scripts/submitPrsc.js
@@ -0,0 +1,26 @@
+const downloadButton = document.querySelector("#download");
+const prscID = document.querySelector("#prscID").textContent.substring(18);
+
+downloadButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    var scrollPos = window.scrollY;
+    window.scroll(0, 0);
+    html2canvas(document.querySelector("article"), {
+        x: 300,
+        width: 900,
+        allowTaint: true
+    }).then(function (canvas) {
+        window.scroll(0, scrollPos);
+        var base64EncodedImg = canvas.toDataURL('image/png');
+        $.ajax({
+            type: "POST",
+            url: "/prscImg",
+            data: { image: base64EncodedImg, prscID: prscID },
+            success: function (res) {
+                console.log(res);
+                var win = window.open();
+                win.location = "/download?id=" + prscID;
+            }
+        });
+    });
+});
