f182e102ee8e3da17b5492bf11517d00d3ae30dc
Aditya D.S <adithya18062000@gmail.com>
2020-06-02 00:06:14 +0530

    removed commented code

--- middleware/index.js
+++ middleware/index.js
@@ -4,7 +4,6 @@ currentUser.isLoggedIn = function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         next();
     } else {
-        // req.flash('error', 'Please Log in before you do that!');
         res.redirect('/');
     }
 };
