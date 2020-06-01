e0453f9a984a49e2a91dcac2f2286cf75f412cd3
Aditya D.S <adithya18062000@gmail.com>
2020-06-02 00:04:35 +0530

    new function to generate kernel from ID

--- controllers/prescription.js
+++ controllers/prescription.js
@@ -35,3 +35,20 @@ exports.getPrescription = async function (id) {
     const prsc = await Prescription.findById(id);
     return await prsc.populate('doctor').execPopulate();
 }
+
+exports.getKernelFromID = function (id) {
+    const len = id.length;
+    var num = 0;
+    var kernel = [];
+
+    id = id.toUpperCase().split("").reverse().join("");
+    for (let i = 0; i < len; i++) {
+        if (!isNaN(Number(id[i])))
+            num = num + Number(id[i]) * Math.pow(24, i);
+        else
+            num = num + id[i].charCodeAt() * Math.pow(24, i);
+    }
+    var numbers = num.toString().substring(0, 10).replace('.', '').split("").map(i => Number(i));
+    kernel.push(numbers.splice(0, 3), numbers.splice(0, 3), numbers.splice(0, 3));
+    return kernel;
+}
