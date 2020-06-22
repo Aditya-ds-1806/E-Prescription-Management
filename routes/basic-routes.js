fcee861469cb817004dee741fbbf40b9a214e463
Aditya D.S <adithya18062000@gmail.com>
2020-06-22 21:01:10 +0530

    added middlewares, new routes for determining role and viewing all prescriptions

--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -6,17 +6,17 @@ const fs = require('fs');
 const user = require('../middleware/index');
 
 const PrescriptionController = require('../controllers/prescription');
-const DoctorController = require('../controllers/doctor');
+const UserController = require('../controllers/user');
 
 router.get('/', function (req, res) {
-    res.render('index', { doctor: req.user, loggedIn: req.isAuthenticated() });
+    res.render('index', { user: req.user, loggedIn: req.isAuthenticated() });
 });
 
-router.get('/verify', user.isLoggedIn, function (req, res) {
+router.get('/verify', user.isLoggedIn, user.isPharmacist, function (req, res) {
     res.render('verify', { user: req.user, loggedIn: req.isAuthenticated() });
 });
 
-router.post('/verify', user.isLoggedIn, function (req, res) {
+router.post('/verify', user.isLoggedIn, user.isPharmacist, function (req, res) {
     const tempPath = __dirname + "//..//temp//";
     const downloadPath = tempPath + "uploaded//";
     const form = formidable({
@@ -47,24 +47,31 @@ router.post('/verify', user.isLoggedIn, function (req, res) {
     });
 });
 
-router.get('/prescription', user.isLoggedIn, user.hasUpdatedDetails, function (req, res) {
+router.get('/prescription', user.isLoggedIn, user.hasUpdatedDetails, user.isDoctor, function (req, res) {
     res.render('form', { user: req.user, loggedIn: req.isAuthenticated() });
 });
 
-router.get('/prescription/:id', user.isLoggedIn, user.hasUpdatedDetails, async function (req, res) {
+router.get('/prescription/:id', user.isLoggedIn, user.isNotPharma, user.ownsPrsc, async function (req, res) {
     const id = req.params.id;
     const prescription = await PrescriptionController.getPrescription(id);
+    if (!prescription) return res.redirect('/');
     res.render('prescription', { prescription: prescription, user: req.user, loggedIn: req.isAuthenticated() });
 });
 
-router.post('/prescription', user.isLoggedIn, user.hasUpdatedDetails, async function (req, res) {
-    const newPrescription = PrescriptionController.generatePrescription(req);
-    const savedPrescription = await PrescriptionController.savePrescription(newPrescription);
-    DoctorController.addNewPrescription(savedPrescription.id, req.user.id);
+router.post('/prescription', user.isLoggedIn, user.hasUpdatedDetails, user.isDoctor, async function (req, res) {
+    const newPrescription = await PrescriptionController.generatePrescription(req);
+    const savedPrescription = await PrescriptionController.savePrescription(newPrescription.prescription);
+    UserController.addNewPrescription(savedPrescription.id, newPrescription.patientId);
+    UserController.addNewPrescription(savedPrescription.id, req.user.id);
+    req.user.prescriptions.push(savedPrescription.id);
     res.redirect('/prescription/' + savedPrescription.id);
 });
 
-router.post('/prscImg', user.isLoggedIn, user.hasUpdatedDetails, async function (req, res) {
+router.get('/prescriptions/list', user.isLoggedIn, user.isNotPharma, function (req, res) {
+    res.render('list', { user: req.user });
+});
+
+router.post('/prscImg', user.isLoggedIn, user.isNotPharma, async function (req, res) {
     const FFTUtils = require('../controllers/fourier/FFTUtils');
     const fileName = req.body.prscID + ".png";
     const tempPath = __dirname + "//..//temp//";
@@ -81,26 +88,28 @@ router.post('/prscImg', user.isLoggedIn, user.hasUpdatedDetails, async function 
     fourierImage.write(savedFilePath);
     console.log("Saved Image");
     res.sendStatus(200);
-    // Compute IFFT
     var originalImage = FFTUtils.reconstruct(fft, [fourierImage.bitmap.width, fourierImage.bitmap.height]);
-    originalImage.write(tempPath + "original.png");
+    originalImage.write(tempPath + "reconstructed.png");
     console.log("Generated original image");
 });
 
-router.get('/download', user.isLoggedIn, user.hasUpdatedDetails, function (req, res) {
+router.get('/download', user.isLoggedIn, user.isNotPharma, user.ownsPrsc, function (req, res) {
     var fileName = "prsc-" + req.query.id + ".png";
-    res.download(__dirname + "//..//temp//" + fileName);
-    setTimeout(() => {
-        fs.unlinkSync(__dirname + "//..//temp//" + fileName);
-        console.log('deleted user copy of prescription');
-    }, 60000); // Delete the sent file after a minute
+    var filePath = __dirname + "//..//temp//" + fileName;
+    if (fs.existsSync(filePath)) {
+        res.download(filePath);
+        setTimeout(() => {
+            fs.unlinkSync(filePath);
+            console.log('deleted user copy of prescription');
+        }, 60000);
+    } else res.redirect('/');
 });
 
-router.get('/profile', user.isLoggedIn, function (req, res) {
-    res.render('profile', { doctor: req.user, loggedIn: req.isAuthenticated() });
+router.get('/profile', user.isLoggedIn, user.isDoctor, function (req, res) {
+    res.render('profile', { user: req.user, loggedIn: req.isAuthenticated() });
 });
 
-router.post('/profile', user.isLoggedIn, function (req, res) {
+router.post('/profile', user.isLoggedIn, user.isDoctor, function (req, res) {
     var details = {
         name: req.body.docName,
         specialisation: req.body.specialisation,
@@ -113,8 +122,19 @@ router.post('/profile', user.isLoggedIn, function (req, res) {
         },
         hasUpdatedDetails: true
     }
-    DoctorController.updateProfile(details, req.user.id);
+    UserController.updateProfile(details, req.user.id);
     res.redirect('/');
 });
+
+router.get('/role', user.isLoggedIn, user.roleIsNotSet, function (req, res) {
+    res.render('role', { user: req.user });
+});
+
+router.post('/role', user.isLoggedIn, user.roleIsNotSet, async (req, res) => {
+    req.user.role = req.body.role;
+    UserController.setRole(req.user.id, req.body.role);
+    if (!req.user.hasUpdatedDetails && req.user.role === 'doctor') return res.sendStatus(201);
+    res.sendStatus(200);
+})
 
 module.exports = router;
