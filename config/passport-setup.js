c4ae19aea1650ab70b1996d52674041eb8e0582b
Aditya D.S <adithya18062000@gmail.com>
2020-06-22 19:58:14 +0530

    naming canges

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,19 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const mongoose = require('mongoose');
-const doctorSchema = require('../models/doctor');
-const DoctorController = require('../controllers/doctor');
+const userSchema = require('../models/user');
+const UserController = require('../controllers/user');
 const keys = require('./keys');
 
-const Doctor = mongoose.model('Doctor', doctorSchema);
+const User = mongoose.model('User', userSchema);
 
 passport.serializeUser(function (user, done) {
-    console.log("serialized ID:", user.id);
     done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-    Doctor.findById(id, function (err, user) {
+    User.findById(id, function (err, user) {
         done(null, user);
     });
 });
@@ -26,7 +25,7 @@ passport.use(
             callbackURL: keys.google.callbackURL
         },
         function (accessToken, refreshToken, profile, done) {
-            DoctorController.addNewDoctor(profile, done);
+            UserController.addNewUser(profile, done);
         }
     )
 );
