28345183a8a70ae965f285aba3b8e180e3c05025
Aditya D.S <adithya18062000@gmail.com>
2020-06-22 20:15:23 +0530

    new middlewares to determine roles

--- middleware/index.js
+++ middleware/index.js
@@ -1,15 +1,37 @@
-var currentUser = {};
+var currentUser = {}
 
 currentUser.isLoggedIn = function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect('/');
-    }
-};
+    if (req.isAuthenticated()) next();
+    else res.redirect('/');
+}
+
+currentUser.isDoctor = function (req, res, next) {
+    if (req.user.role === 'doctor') next()
+    else res.redirect('back')
+}
+
+currentUser.isPharmacist = function (req, res, next) {
+    if (req.user.role === 'pharmacist') next()
+    else res.redirect('/')
+}
+
+currentUser.isNotPharma = function (req, res, next) {
+    if (req.user.role !== 'pharmacist') next()
+    else res.redirect('/')
+}
 
 currentUser.hasUpdatedDetails = function (req, res, next) {
-    if (req.user.hasUpdatedDetails) next();
+    if (req.user.hasUpdatedDetails) next()
+    else res.redirect('/');
+}
+
+currentUser.roleIsNotSet = function (req, res, next) {
+    if (req.user.role === null) next()
+    else res.redirect('/');
+}
+
+currentUser.ownsPrsc = function (req, res, next) {
+    if (req.user.prescriptions.includes(req.query.id || req.params.id)) next();
     else res.redirect('/');
 }
 
