c8a52369b9d86f8c46499e9160323cef5be0bba1
Aditya DS <adithya18062000@gmail.com>
2023-01-09 23:11:26 +0530

    fix: upload folder path

--- controllers/onServerStart.js
+++ controllers/onServerStart.js
@@ -5,13 +5,14 @@ var onServerStart = {};
 
 onServerStart.createTempFolder = function () {
     var tempPath = path.join(__dirname, "//..//temp");
+    var uploadedFolderPath = path.join(tempPath, "//uploaded");
     const dirExists = fs.existsSync(tempPath);
     if (!dirExists) {
         fs.mkdir(tempPath, () => console.log("Created Temp folder"));
-        fs.mkdir(tempPath + "//uploaded", () => console.log("temp/uploaded folder created"));
+        fs.mkdir(uploadedFolderPath, () => console.log("temp/uploaded folder created"));
         setTimeout(() => {
             console.log(fs.readdirSync(tempPath));
-        }, 1000);
+        }, 5000);
     };
 }
 
