83721bd46405216b6da3f2e032d2a92c9130560c
Aditya DS <adithya18062000@gmail.com>
2023-01-09 23:08:45 +0530

    fix: startup script

--- controllers/onServerStart.js
+++ controllers/onServerStart.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 
 var onServerStart = {};
 
 onServerStart.createTempFolder = function () {
-    var tempPath = __dirname + "//..//temp";
-    fs.exists(tempPath, (dirExists) => {
-        if (!dirExists) {
-            fs.mkdir(tempPath, () => console.log("Created Temp folder"));
-            fs.mkdir(tempPath + "//uploaded", () => console.log("temp/uploaded folder created"));
-        }
-    });
+    var tempPath = path.join(__dirname, "//..//temp");
+    const dirExists = fs.existsSync(tempPath);
+    if (!dirExists) {
+        fs.mkdir(tempPath, () => console.log("Created Temp folder"));
+        fs.mkdir(tempPath + "//uploaded", () => console.log("temp/uploaded folder created"));
+        setTimeout(() => {
+            console.log(fs.readdirSync(tempPath));
+        }, 1000);
+    };
 }
 
 
